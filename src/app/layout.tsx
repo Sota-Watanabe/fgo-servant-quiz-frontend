0f92baad230f7b6b6b45a075d416dcc62cbb9fe7
Sota-Watanabe <stronger.riceball@gmail.com>
2025-09-21 23:46:46 +0900

    feat: Google Analyticsコンポーネントを追加し、トラッキング機能を実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./components/QueryProvider";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <GoogleAnalytics measurementId="G-R5XW321DL4" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6714287536670840"
